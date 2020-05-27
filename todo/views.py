1a75c9f83b481bd1f0a3945d6e5a5a9436374173
Siddharth Murugan <mksiddhu2@gmail.com>
2020-05-27 08:17:47 +0530

    Created signup page

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 def signupuser(request):
-	return render(request, 'todo/signupuser.html')
+	if request.method == 'GET':
+		return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+	else:
+		#Creating new user
+		User.objects.create_user() 
