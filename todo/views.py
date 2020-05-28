c65906bdc167060d6cdb2e36b139af6e4034e02b
Siddharth Murugan <mksiddhu2@gmail.com>
2020-05-28 09:37:41 +0530

    User login page is created

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError 
+from django.contrib.auth import login
 
 def signupuser(request):
 	if request.method == 'GET':
 		return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
 	else:
 		#Creating new user
-		User.objects.create_user() 
+		if request.POST['password1'] == request.POST['password2']:
+			try:
+				user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+				user.save()
+				login(request, user)
+				return redirect('currenttodos')
+			except IntegrityError:
+				return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Username already exist'})
+		else:
+			return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Password didnot match'})
+
+def currenttodos(request):
+	return render(request, 'todo/currenttodos.html')
